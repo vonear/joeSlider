92840143c609d29882d75a537102adee1ff39b82
joe511230 <itsfantacy@hotmail.com>
2016-02-23 18:01:45 +0800

    add togglebar
    add a single toggle bar ,its position is under the slide main area

--- JoeSlider.js
+++ JoeSlider.js
@@ -1,17 +1,18 @@
 (function($) {
     $.fn.JoeSlider = function(params) {
         var options = {
-            "startItem": 1, 		//起始位置
-            "autoPlay": true,		//是否自动播放
-            "interval": 3000,		//播放间隔
-            "slideDuration": 350,	//切换时间
-            "direction": "h", 		//切换方向 ，"h" , "v",
-            "controller": "true",	//是否手动控制切换
-            "nextBtn": "joe-slider-next",	//默认下一个切换按钮,可自定义为DOM,JQUERY对象或选择器
-            "prevBtn": "joe-slider-prev",	//默认上一个切换按钮
-            "nextBtnText": "next",			//下一个按钮文字
-            "prevBtnText": "prev",			//上一个按钮文字
-            "pauseWhenHover": "true"		//鼠标悬浮时，是否暂停播放
+            "startItem": 1, //起始位置
+            "autoPlay": true, //是否自动播放
+            "interval": 3000, //播放间隔
+            "slideDuration": 350, //切换时间
+            "direction": "h", //切换方向 ，"h" , "v",
+            "controller": "true", //是否手动控制切换
+            "nextBtn": "joe-slider-next", //默认下一个切换按钮,可自定义为DOM,JQUERY对象或选择器
+            "prevBtn": "joe-slider-prev", //默认上一个切换按钮
+            "nextBtnText": "next", //下一个按钮文字
+            "prevBtnText": "prev", //上一个按钮文字
+            "pauseWhenHover": "true", //鼠标悬浮时，是否暂停播放
+            "toggleBar": "true" //显示切换条
         };
         try {
             $.extend(options, params);
@@ -61,20 +62,21 @@
             /*切换事件*/
             var timer, checkCloneTimer;
             var doSlide = function(isNext) {
-                var curItem = slider.find(".cur"), 		//取得下一个滑块，并设置相关属性
+                var curItem = slider.find(".cur"), //取得下一个滑块，并设置相关属性
                     curIndex = curItem.index();
                 curItem.removeClass('cur');
-                if (options.direction == "h") {			//水平切换
+                if (options.direction == "h") { //水平切换
                     slider.css({
                         "transform": "translateX(" + (-1 * (!isNext ? curIndex - 1 : curIndex + 1) * itemW) + "px)",
                         "transition": "all linear " + options.slideDuration / 1000 + "s"
                     });
-                } else if (options.direction == "v") {	//垂直切换
+                } else if (options.direction == "v") { //垂直切换
                     slider.css({
                         "transform": "translateY(" + (-1 * (!isNext ? curIndex - 1 : curIndex + 1) * itemH) + "px)",
                         "transition": "all linear " + options.slideDuration / 1000 + "s"
                     });
                 }
+
                 clearTimeout(checkCloneTimer);
                 if (isNext) {
                     curItem.next().addClass('cur');
@@ -103,7 +105,7 @@
                         if (nextItemIndex != curItem.prev().index()) {
                             curItem.prev().removeClass('cur');
                             items.last().addClass('cur');
-                            if (options.direction == "h") {//判断是否由第一个节点切换至最后一个的克隆节点，如果是，切换完成后切换至真实的最后一个节点
+                            if (options.direction == "h") { //判断是否由第一个节点切换至最后一个的克隆节点，如果是，切换完成后切换至真实的最后一个节点
                                 slider.css({
                                     "transform": "translateX(" + (-1 * itemCount * itemW) + "px)",
                                     "transition": "none"
@@ -117,10 +119,20 @@
                         }
                     }, options.slideDuration);
                 }
+
+                /*如果存在手动切换，自动切换时一并切换手动控制条*/
+
+                if(options.toggleBar){
+                	var toggleBar = sliderCtn.parent().find(".toggle-bar-ctn .toggle-bar");
+                	toggleBar.filter(".cur-bar").removeClass('cur-bar').css('background', '#d0d1d3');
+                	var itemIndexAfterSlide = slider.find(".cur").attr("data-index");
+                	toggleBar.slice(itemIndexAfterSlide-1, itemIndexAfterSlide).addClass('cur-bar').css('background', '#3e98f0');
+                }
+
             };
 
             /*自动播放*/
-            if (options.autoPlay) { 
+            if (options.autoPlay) {
                 timer = setInterval(function() { doSlide(true) }, options.interval);
             }
 
@@ -180,6 +192,51 @@
                     doSlide(true);
                 });
 
+            }
+            /*切换控制条，可切换至指定的节点*/
+            if (options.toggleBar) {
+                var toggleBarCtnCss = {
+                    "width": itemW,
+                    "z-index": "100"
+                };
+                var toggleBarCss = {
+                    "height": "3px",
+                    "background": "#d0d1d3",
+                    "cursor": "pointer",
+                    "float": "left"
+                };
+                var sliderCtnWidthToggleBar = $("<div>").css({
+                    "overflow": "visible",
+                    "width": itemW,
+                    "height": itemH
+                }).appendTo(sliderCtn.parent());
+                sliderCtn.appendTo(sliderCtnWidthToggleBar);
+
+                var toggleBarCtn = $("<div class='toggle-bar-ctn'>").css(toggleBarCtnCss).appendTo(sliderCtnWidthToggleBar);
+                var toggleBarW = itemW / itemCount;
+                for (var i = 0; i < itemCount; i++) {
+                   	var toggleBarTemp =  $("<div>").css({ "width": toggleBarW }).css(toggleBarCss)
+                    .attr("data-target-index", i + 1).addClass('toggle-bar '+ (i==(options.startItem-1)? "cur-bar" : ""))
+                    .appendTo(toggleBarCtn);
+                   i==(options.startItem-1) ? toggleBarTemp.css({"background":"#3e98f0"}) : "";
+                }
+
+                toggleBarCtn.click(function(event) {
+                    clearInterval(timer);
+                    toggleBarCtn.find(".cur-bar").removeClass('cur-bar').css('background', '#d0d1d3');
+                    
+                    var target = event.target || event.srcElement;
+                    target = $(target).css({"background":"#3e98f0"}).addClass('cur-bar');
+
+                    var targetIndex = target.attr("data-target-index");
+                    slider.find(".cur").removeClass('cur');
+                    items.slice(targetIndex - 1, targetIndex).addClass('cur');
+                    slider.css({
+                        "transform": "translateX(" + (-1 * targetIndex * (options.direction == "h" ? itemW : itemH)) + "px)",
+                        "transition": "all linear " + options.slideDuration / 1000 + "s"
+                    });
+                    timer = setInterval(function() { doSlide(true) }, options.interval);
+                });
             }
 
 
